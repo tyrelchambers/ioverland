dda7b84003155681b6742724f0b23de92665dc9a
Tyrel Chambers <tychambers3@gmail.com>
2024-01-03 23:09:22 -0500

    index and explore

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -3,12 +3,39 @@ import React, { useEffect } from "react";
 import { Button } from "./ui/button";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
+import { useViewportWidth } from "@/hooks/useViewportWidth";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+import { Home, LayoutDashboard, Mountain } from "lucide-react";
+
+const routes = [
+  {
+    href: "/",
+    label: "Home",
+    icon: <Home size={20} />,
+  },
+  {
+    href: "/explore",
+    label: "Explore",
+    icon: <Mountain size={20} />,
+  },
+];
+
+const authRoutes = [
+  {
+    href: "/dashboard",
+    label: "Dashboard",
+    icon: <LayoutDashboard size={20} />,
+  },
+];
 
 interface Props {
   on?: "dark" | "light";
+  className?: string;
 }
 
-const Header = ({ on }: Props) => {
+const Header = ({ on, className }: Props) => {
+  const { width } = useViewportWidth();
+
   useEffect(() => {
     const header = document.querySelector(".header");
 
@@ -24,34 +51,85 @@ const Header = ({ on }: Props) => {
   }, []);
 
   return (
-    <header className="header sticky top-0 p-4 bg-background transition-all flex items-center z-50 border-b border-border">
+    <header
+      className={cn(
+        "header sticky top-0 p-4 bg-background transition-all flex items-center z-50 border-b border-border",
+        className
+      )}
+    >
       <h2 className="text-foreground font-bold font-serif">iOverland</h2>
-      <div className="flex gap-4 items-center ">
-        <SignedIn>
-          <Link href={`/explore`}>
-            <Button variant="link">Explore</Button>
-          </Link>
-          <Link href={`/dashboard`}>
-            <Button variant="link">Dashboard</Button>
-          </Link>
-          <Link href="/build/new">
-            <Button
-              size="sm"
-              className={cn({
-                "text-primary-foreground bg-primary": on === "dark",
-              })}
+      {width <= 768 ? (
+        <Sheet>
+          <SheetTrigger>
+            <div className="w-[20px] h-[15px] flex flex-col gap-1">
+              <span className="w-full h-[2px] bg-foreground"></span>
+              <span className="w-full h-[2px] bg-foreground"></span>
+              <span className="w-1/2 h-[2px] bg-foreground"></span>
+            </div>
+          </SheetTrigger>
+          <SheetContent>
+            <div className="flex flex-col gap-8 mt-10">
+              {routes.map((route) => (
+                <Link
+                  href={route.href}
+                  key={route.label}
+                  className="flex gap-6 items-center underline hover:text-primary"
+                >
+                  {route.icon} {route.label}
+                </Link>
+              ))}
+              {authRoutes.map((route) => (
+                <Link
+                  href={route.href}
+                  key={route.label}
+                  className="flex gap-6 items-center underline hover:text-primary"
+                >
+                  {route.icon} {route.label}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      ) : (
+        <div className="flex gap-8 items-center ">
+          {routes.map((route) => (
+            <Link
+              href={`/explore`}
+              key={route.label}
+              className="flex gap-3 items-center  hover:text-primary text-foreground/50 hover:text-foreground"
             >
-              New build
-            </Button>
-          </Link>
-          <UserButton afterSignOutUrl="/" />
-        </SignedIn>
-        <SignedOut>
-          <Link href="/sign-up">
-            <Button>Sign up</Button>
-          </Link>
-        </SignedOut>
-      </div>
+              {route.label}
+            </Link>
+          ))}
+          {authRoutes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.label}
+              className="flex gap-3 items-center  hover:text-primary text-foreground/50 hover:text-foreground"
+            >
+              {route.label}
+            </Link>
+          ))}
+          <SignedIn>
+            <Link href="/build/new">
+              <Button
+                size="sm"
+                className={cn({
+                  "text-primary-foreground bg-primary": on === "dark",
+                })}
+              >
+                New build
+              </Button>
+            </Link>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Link href="/sign-up">
+              <Button>Sign up</Button>
+            </Link>
+          </SignedOut>
+        </div>
+      )}
     </header>
   );
 };
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,10 +1,7 @@
 import Header from "@/components/Header";
-import { H1, H2 } from "@/components/Heading";
+import { H2 } from "@/components/Heading";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import { UserButton } from "@clerk/nextjs";
-import { Bookmark, Lightbulb, Video, Wrench } from "lucide-react";
+import { Lightbulb, Video, Wrench } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,54 +9,30 @@ export default function Home() {
   return (
     <main>
       <section className="h-screen flex relative">
-        <div className="w-full flex flex-col items-center absolute inset-0 z-10 justify-center max-w-screen-xl h-screen mx-auto">
-          <header className="header absolute top-0 p-4 transition-all flex items-center z-50 w-full">
-            <h2 className="text-foreground font-bold font-serif">iOverland</h2>
-            <div className="flex gap-4 items-center ">
-              <SignedIn>
-                <Link href={`/explore`}>
-                  <Button variant="link">Explore</Button>
-                </Link>
-                <Link href={`/dashboard`}>
-                  <Button variant="link">Dashboard</Button>
-                </Link>
-                <Link href="/build/new">
-                  <Button
-                    size="sm"
-                    className={cn("text-primary-foreground bg-primary")}
-                  >
-                    New build
-                  </Button>
-                </Link>
-                <UserButton afterSignOutUrl="/" />
-              </SignedIn>
-              <SignedOut>
-                <Link href="/sign-up">
-                  <Button>Sign up</Button>
-                </Link>
-              </SignedOut>
+        <div className="w-full flex flex-col items-center absolute inset-0 z-10 justify-center max-w-screen-xl h-full min-h-screen mx-auto">
+          <Header className="absolute top-0 bg-transparent border-0 w-full" />
+          <div className="flex flex-col p-4 w-full items-center">
+            {" "}
+            <h1 className="font-serif lg:text-8xl text-4xl mb-10 font-bold text-foreground text-center">
+              More than Overland
+            </h1>
+            <p className="lg:text-2xl text-muted-foreground font-light max-w-2xl leading-relaxed text-center text-pretty">
+              Showcase your overlanding builds. But it doesn&apos;t stop there.
+              Show everyone your offroad builds from trucks, to SxS and ATVs.
+            </p>
+            <div className="flex items-center mt-10 gap-6 flex-col lg:flex-row">
+              <Link href="/sign-up">
+                <Button type="button" className="w-full">
+                  Create your build
+                </Button>
+              </Link>
+
+              <Link href="/explore">
+                <Button type="button" variant="outline">
+                  Find your inspiration
+                </Button>
+              </Link>
             </div>
-          </header>
-          <h1 className="font-serif text-8xl mb-10 font-bold text-foreground text-center">
-            More than Overland
-          </h1>
-          <p className="text-2xl text-muted-foreground font-light max-w-2xl leading-relaxed text-center">
-            Showcase your overlanding builds. But it doesn&apos;t stop there.
-            Show everyone your offroad builds from trucks, to SxS and ATVs.
-          </p>
-
-          <div className="flex items-center mt-10 gap-6">
-            <Link href="/sign-up">
-              <Button type="button" className="w-full">
-                Create your build
-              </Button>
-            </Link>
-
-            <Link href="/explore">
-              <Button type="button" variant="outline">
-                Find your inspiration
-              </Button>
-            </Link>
           </div>
         </div>
 
@@ -73,21 +46,27 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="max-w-screen-xl mx-auto  -translate-y-40">
-        <div className="my-10 rounded-md shadow-xl overflow-hidden">
-          <div className="relative w-full h-[600px]">
-            <Image src="/tease 2.jpg" alt="" fill className="object-cover" />
+      <section className="max-w-screen-xl mx-auto  lg:-translate-y-40">
+        <div className="my-10 rounded-md shadow-xl overflow-hidden mx-4">
+          <div className="relative w-full lg:h-[600px] h-[200px] ">
+            <Image
+              src="/tease 2.jpg"
+              alt=""
+              fill
+              className="object-cover"
+              priority
+            />
           </div>
         </div>
 
         <div className="flex flex-col items-center w-full py-20">
-          <H2 className="!text-6xl">Show off your vehicle</H2>
-          <p className="text-2xl text-muted-foreground font-light max-w-2xl leading-relaxed text-center mt-4">
+          <H2 className="lg:!text-6xl text-center">Show off your vehicle</H2>
+          <p className="lg:text-2xl text-muted-foreground font-light max-w-2xl leading-relaxed text-center mt-4">
             Upload images and videos of your Overlanding build to showcase to
             the world.
           </p>
 
-          <div className="grid grid-cols-3 gap-10 mt-20">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 mt-20">
             <div className="flex flex-col items-center text-muted-foreground gap-6">
               <Video size={30} />
               <h3>Upload videos and images</h3>
@@ -100,35 +79,6 @@ export default function Home() {
               <Lightbulb size={30} />
               <h3>Become someone&apos;s inspiration</h3>
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="relative">
-        <div className="relative h-screen">
-          <Image
-            src="/topo2.jpg"
-            fill
-            alt=""
-            className="object-cover opacity-10"
-          />
-        </div>
-
-        <div className="flex flex-col items-center w-full py-20 absolute inset-0 max-w-screen-lg mx-auto">
-          <H2 className="!text-6xl mb-20">Become an inspiration</H2>
-
-          <div className="w-1/2 h-[300px] mr-auto flex flex-col justify-center items-center gap-4 bg-background my-10 p-8 rounded-md shadow-lg text-muted-foreground">
-            <Lightbulb size={40} />
-            <h3 className="text-xl text-center">
-              Through likes &amp; views, become discoverable
-            </h3>
-          </div>
-
-          <div className="w-1/2 h-[300px] ml-auto flex flex-col justify-center items-center gap-4 bg-background my-10 p-8 rounded-md shadow-lg text-muted-foreground">
-            <Bookmark size={40} />
-            <h3 className="text-xl text-center">
-              Bookmark your favourite builds for later
-            </h3>
           </div>
         </div>
       </section>
