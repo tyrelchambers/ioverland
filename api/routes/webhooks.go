1dcb058964a4256341838e81d2b9371374d81444
Tyrel Chambers <tychambers3@gmail.com>
2024-01-04 20:50:32 -0500

    update webhook

--- api/routes/webhooks.go
+++ api/routes/webhooks.go
@@ -2,16 +2,21 @@ package routes
 
 import (
 	"api/db"
+	"api/domain/build"
 	"api/domain/user"
 	"api/utils"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/clerkinc/clerk-sdk-go/clerk"
 	"github.com/gin-gonic/gin"
 	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/customer"
+	"github.com/stripe/stripe-go/webhook"
 	svix "github.com/svix/svix-webhooks/go"
 )
 
@@ -94,65 +99,66 @@ func Webhooks(c *gin.Context) {
 }
 
 func StripeWebhooks(c *gin.Context) {
-	// stripe_key := utils.GoDotEnvVariable("STRIPE_TEST_KEY")
-	// stripe.Key = stripe_key
+	stripe_key := utils.GoDotEnvVariable("STRIPE_TEST_KEY")
+	stripe.Key = stripe_key
 
-	// const MaxBodyBytes = int64(65536)
-	// c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, MaxBodyBytes)
-	// payload, err := io.ReadAll(c.Request.Body)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error reading request body: %v\n", err)
-	// 	c.String(400, "Bad Request")
-	// 	return
-	// }
+	const MaxBodyBytes = int64(65536)
+	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, MaxBodyBytes)
+	payload, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading request body: %v\n", err)
+		c.String(400, "Bad Request")
+		return
+	}
 
-	// event := stripe.Event{}
+	event := stripe.Event{}
 
-	// if err := json.Unmarshal(payload, &event); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "⚠️  Webhook error while parsing basic request. %v\n", err.Error())
-	// 	c.String(400, "Bad Request")
-	// 	return
-	// }
+	if err := json.Unmarshal(payload, &event); err != nil {
+		fmt.Fprintf(os.Stderr, "⚠️  Webhook error while parsing basic request. %v\n", err.Error())
+		c.String(400, "Bad Request")
+		return
+	}
 
-	// endpointSecret := utils.GoDotEnvVariable("STRIPE_WH_SECRET")
-	// signatureHeader := c.Request.Header.Get("Stripe-Signature")
-	// event, err = webhook.ConstructEvent(payload, signatureHeader, endpointSecret)
+	endpointSecret := utils.GoDotEnvVariable("STRIPE_WH_SECRET")
+	signatureHeader := c.Request.Header.Get("Stripe-Signature")
+	event, err = webhook.ConstructEvent(payload, signatureHeader, endpointSecret)
 
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "⚠️  Webhook signature verification failed. %v\n", err)
-	// 	c.String(400, "Bad Request")
-	// 	return
-	// }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "⚠️  Webhook signature verification failed. %v\n", err)
+		c.String(400, "Bad Request")
+		return
+	}
 
-	// switch event.Type {
-	// case "customer.created":
-	// 	var customerData stripe.Customer
+	switch event.Type {
+	case "customer.subscription.deleted":
+		var subscriptionData stripe.Subscription
 
-	// 	if err := json.Unmarshal(event.Data.Raw, &customerData); err != nil {
-	// 		fmt.Fprintf(os.Stderr, "⚠️  Webhook error while parsing basic request. %v\n", err.Error())
-	// 		c.String(400, "Bad Request")
-	// 		return
-	// 	}
+		if err := json.Unmarshal(event.Data.Raw, &subscriptionData); err != nil {
+			fmt.Fprintf(os.Stderr, "⚠️  Webhook error while parsing basic request. %v\n", err.Error())
+			c.String(400, "Bad Request")
+			return
+		}
 
-	// 	// we do this because if they cancel their plan we need to delete their user at the end of the billing cycle
-	// case "customer.subscription.deleted":
-	// 	var subscriptionData stripe.Subscription
+		usr, err := user.FindUserByCustomerId(db.Client, subscriptionData.Customer.ID)
 
-	// 	if err := json.Unmarshal(event.Data.Raw, &subscriptionData); err != nil {
-	// 		fmt.Fprintf(os.Stderr, "⚠️  Webhook error while parsing basic request. %v\n", err.Error())
-	// 		c.String(400, "Bad Request")
-	// 		return
-	// 	}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "⚠️  Webhook error while parsing basic request for event: %v. %v\n", event.Type, err.Error())
+			c.String(400, "Bad Request")
+			return
+		}
 
-	// 	usr, err := user.FindUserByCustomerId(db.Client, subscriptionData.Customer.ID)
+		builds, err := build.AllByUser(db.Client, usr.Uuid)
 
-	// 	if err != nil {
-	// 		fmt.Fprintf(os.Stderr, "⚠️  Webhook error while parsing basic request for event: %v. %v\n", event.Type, err.Error())
-	// 		c.String(400, "Bad Request")
-	// 		return
-	// 	}
+		if err != nil {
+			fmt.Println("Error getting builds in Stripe webhook event: ", err)
+			c.String(500, "Something went wrong")
+			return
+		}
 
-	// 	// usr.Delete(db.Client)
-	// }
+		for _, build := range builds[1:] {
+			build.Private = true
+			build.Update(db.Client)
+		}
+	}
 	c.String(200, "success")
 }
