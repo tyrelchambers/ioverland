37173651a9dab83d70b913ecb02484348d0b7e0b
Tyrel Chambers <tychambers3@gmail.com>
2024-01-06 22:50:24 -0500

    .

--- api/controllers/explore.go
+++ api/controllers/explore.go
@@ -24,33 +24,6 @@ func Explore() (ExploreRes, error) {
 		return ExploreRes{}, err
 	}
 
-	top_10_likes, err := build.GetTop10ByLikes(db.Client)
-	top_10_views, err := build.GetTop10ByViews(db.Client)
-
-	if err != nil {
-		return ExploreRes{}, err
-	}
-
-	for i := 0; i < 5; i++ {
-		randInt := rand.Intn(len(top_10_likes))
-		res.Top10 = append(res.Top10, top_10_likes[randInt])
-	}
-
-	for i := 0; i < 5; i++ {
-		randInt := rand.Intn(len(top_10_views))
-		res.Top10 = append(res.Top10, top_10_views[randInt])
-	}
-
-	for i := 0; i < 2; i++ {
-		randInt := rand.Intn(len(top_10_likes))
-		res.Featured = append(res.Featured, top_10_likes[randInt])
-	}
-
-	for i := 0; i < 2; i++ {
-		randInt := rand.Intn(len(top_10_views))
-		res.Featured = append(res.Featured, top_10_views[randInt])
-	}
-
 	rand.Shuffle(len(res.Top10), func(i, j int) { res.Top10[i], res.Top10[j] = res.Top10[j], res.Top10[i] })
 
 	if 20-int(count) < 0 {
