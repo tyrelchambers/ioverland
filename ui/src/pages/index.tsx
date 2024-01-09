133dda7b5bc48441b62b417e2b0d765915990204
Tyrel Chambers <tychambers3@gmail.com>
2024-01-09 00:48:12 -0500

    fix padding

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -98,7 +98,7 @@ export default function Home() {
         <div className="lg:h-[600px] lg:w-[600px] w-full h-[400px]  relative overflow-hidden rounded-xl shadow-xl mb-10 lg:mb-0">
           <Image src="/car by water.jpg" alt="" fill className="object-cover" />
         </div>
-        <div className="flex-1 flex flex-col px-20">
+        <div className="flex-1 flex flex-col lg:px-20 px-0">
           <header className="max-w-2xl w-full">
             <H2 className="mb-5 text-4xl">Features</H2>
             <p className="text-muted-foreground">
